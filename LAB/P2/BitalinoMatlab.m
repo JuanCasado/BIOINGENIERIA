ee0bba7f76ee5f465f7432d1d6db1afc19c78fae
JuanCasado <juan.casado@edu.uah.es>
2020-02-21 11:04:35 +0100

    histogram

--- LAB/P2/BitalinoMatlab.m
+++ LAB/P2/BitalinoMatlab.m
@@ -19,19 +19,33 @@ delete(b)
 %%
 % Open an existing recording
 load('./measure2/formated_data.txt'); % Comment this line to use live data
-ECG_raw = data(:,8);
+ECG_raw = formated_data(:,8);
 figure;
 plot(ECG_raw);
 
 %%
 % Find the peacks
 
-[peaks, x_values] = findpeaks(ECG_raw,'MinPeakHeight', 900);
+[peaks, x_values] = findpeaks(ECG_raw,'MinPeakHeight', 800);
 
 plot(ECG_raw)
 hold on
 plot (x_values,peaks,'o')
 hold off
 
+%%
+% Calculate time intervals
+
+previous_time = 0;
+time_intervals = [];
+for i = 1:size(x_values,1)
+   time_intervals = [time_intervals, x_values(i) - previous_time];
+   previous_time = x_values(i);
+end
+
+%%
+hist(time_intervals)
+%%
+bar (time_intervals)
 
 
